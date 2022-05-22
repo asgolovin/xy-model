e972b4b97ac34bdd6b0de338e1aafa1ae7514561
Anastasia Golovin <asgolovin@protonmail.com>
2022-05-22 20:13:41 +0200

    Rename file

--- src/main.jl
+++ src/main.jl
@@ -1,6 +1,6 @@
 include("data/lattice.jl")
 include("data/params.jl")
-include("simulation/simulator.jl")
+include("simulation/simulation.jl")
 
 params = Params(nrow = 100, ncol = 100)
 consts = Consts()
