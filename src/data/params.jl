65dff5352defd598a421aa63608ef5541b5a6345
Anastasia Golovin <asgolovin@protonmail.com>
2022-05-22 14:56:58 +0200

    Define simulation params

--- src/data/params.jl
+++ src/data/params.jl
@@ -1,5 +1,10 @@
 struct Params
-    T::Real
-    N::Integer
-    M::Integer
+    T::AbstractFloat     # temperature
+    J::AbstractFloat     # coupling constant
+    nrow::Integer        # number of rows in the lattice
+    ncol::Integer        # number of cols in the lattice
+end
+
+struct Consts
+    kB = 1.380649e-23    # Boltzmann constant
 end
